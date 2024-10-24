4cf7d554be414ac788d978714b685580c36a6277
savana64 <xenawarrior@centrum.cz>
2024-10-24 13:16:27 +0200

    modely , views a admin

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 # Create your views here.¨
 def index(request):
     return HttpResponse("Здраствуй Мыр. You are at the polls index.")
+
+def results(request, question_id):
+    response = 'Hledíš na rizalts of kvesčn %s'
+    return HttpResponse(response % question_id)
+
+def detail(request, question_id):
+    return HttpResponse('Koukáš na otázku %s.' % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('júá voting on question %s' % question_id)
